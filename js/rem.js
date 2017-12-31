775a349f01aa57a9bce8f5bf04effe9e6cf1b39e
LiiYuFeng <31206582+LiiYuFeng@users.noreply.github.com>
2017-12-31 14:23:02 +0800

    Add files via upload

--- js/rem.js
+++ js/rem.js
@@ -1,35 +1,28 @@
-/*rem自适应*/
+/*px转义为rem*/
 (function(doc, win) {
-            var docEl = doc.documentElement,
-                resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-                recalc = function() {
-                    var clientWidth = docEl.clientWidth;
-                    if (!clientWidth) return;
-                    if (clientWidth >= 640) {
-                        docEl.style.fontSize = '100px';
-                    } else {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function() {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if (clientWidth >= 640) {
+                     docEl.style.fontSize = '100px';
+                } else {
                         docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
-                    }
-                };
-            if (!doc.addEventListener) return;
-            win.addEventListener(resizeEvt, recalc, false);
-            doc.addEventListener('DOMContentLoaded', recalc, false);
+                }
+            };
+                if (!doc.addEventListener) return;
+                win.addEventListener(resizeEvt, recalc, false);
+                doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 /*wow动画*/
 var wow = new WOW({
-    boxClass: 'wow',
-    animateClass: 'animated',
-    offset: 0,
-    mobile: true,
-    live: true
-});
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: true,
+            live: true
+        });
 wow.init();
-/*底部字体颜色*/
-var colorFont=['red','yellow','white'];
-var i=0;
-function color(){
-        i+=1;
-        if (i>=3) {i=0};
-        $('.redLink a').css('color',colorFont[i])
-        };
-setInterval(color,1000);
+
+
